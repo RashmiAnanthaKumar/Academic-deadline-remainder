6d455bc65a469252c47509b8030acd3568e1e66d
Rashmi <rashmi.ananthakumar2006@gmail.com>
2026-03-25 14:18:25 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,14 @@ require("dotenv").config();
 
 const app = express();
 
+// ✅ CORS FIX (IMPORTANT)
+app.use(cors({
+  origin: "*", // allow all (for now)
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -15,29 +21,24 @@ const authRoutes = require("./routes/authRoutes");
 const submissionRoutes = require("./routes/submissionRoutes");
 
 app.use("/api/submissions", submissionRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
 
 // serve files
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api/tasks", taskRoutes);
-app.use("/api/auth", authRoutes);
-
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
-
-
-
 });
 
-// MongoDB (optional)
+// MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log("MongoDB Error ❌:", err.message));
 
 // Start server
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
